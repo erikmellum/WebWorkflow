59decfc1df1ec17f06b99f8e4f1d50fdd2b6262c
ErikMellum <mellum.erik@gmail.com>
2014-08-17 00:47:10 -0700

    Trying to add LiveReload to Express server

--- gulpfile.js
+++ gulpfile.js
@@ -6,13 +6,16 @@ var gulp = require ('gulp'),
     sass = require('gulp-ruby-sass'),
     coffee = require('gulp-coffee'),
     lr = require('tiny-lr'),
+    autoprefixer = require('gulp-autoprefixer'),
+    rename = require('gulp-rename'),
+    minifycss = require('gulp-minify-css'),
     server = lr();
 
 var jsSources = [
   'components/scripts/*'
 ];
 
-var sassSources = [
+var styleSources = [
   'components/sass/*'
 ];
 
@@ -24,10 +27,35 @@ var viewSources = [
   'views/index.ejs'
 ];
 
+function notifyLiveReload(event) {
+  var fileName = require('path').relative(__dirname, event.path);
+
+  tinylr.changed({
+    body: {
+      files: [fileName]
+    }
+  });
+}
+
+var tinylr;
+gulp.task('livereload', function() {
+  tinylr = require('tiny-lr')();
+  tinylr.listen(4002);
+});
+
+gulp.task('express', function() {
+  var express = require('express');
+  var app = express();
+  app.use(require('connect-livereload')({port: 4002}));
+  app.use(express.static(__dirname));
+  app.listen(4000);
+});
+
 gulp.task('js', function() {
   gulp.src(jsSources)
   .pipe(uglify())
   .pipe(concat('script.js'))
+  .pipe(livereload())
   .pipe(gulp.dest('public/javascripts'));
 });
 
@@ -42,24 +70,31 @@ gulp.task('watch', function(){
   var server = livereload();
   gulp.watch(jsSources, ['js']);
   gulp.watch(coffeeSources, ['coffee']);
-  gulp.watch(sassSources, ['sass']);
-  gulp.watch(['public/javascripts/script.js','*.html'], function(e){
-    server.changed(e.path);
-  });
+  gulp.watch(styleSources, ['styles']);
+  gulp.watch(viewSources, notifyLiveReload);
+  gulp.watch('views/*.ejs', notifyLiveReload);
+  gulp.watch('public/stylesheets/*.css', notifyLiveReload);
+  gulp.watch('public/javascripts/*.js', notifyLiveReload);
+  gulp.watch('*.html', notifyLiveReload);
 })
 
-gulp.task('default', ['sass', 'js', 'views', 'watch', 'coffee'])
+gulp.task('default', ['styles', 'js',  'express', 'livereload', 'watch', 'coffee']);
 
-gulp.task('sass', function(){
-  gulp.src(sassSources)
+gulp.task('styles', function(){
+  gulp.src(styleSources)
   .pipe(sass({style: 'expanded', lineNumbers: true}))
     .on('error', gutil.log)
   .pipe(concat('style.css'))
+  .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1'))
   .pipe(gulp.dest('public/stylesheets'))
-  .pipe(livereload());
+  .pipe(rename({suffix: '.min'}))
+  .pipe(minifycss())
+  .pipe(livereload())
+  .pipe(gulp.dest('public/stylesheets'));
 });
 
 gulp.task('views', function(){
   gulp.src(viewSources)
-  .pipe(livereload());
+  .pipe(livereload())
+  .pipe(gulp.dest('views'));
 });
--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/about', function(req, res) {
+  res.render('index', { title: 'About' });
+});
+
 module.exports = router;
+
